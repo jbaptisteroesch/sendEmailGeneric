ac6f1097d27d406201c9b7a2c0cf5c9ce3250119
JBaptiste Roesch <jean-baptiste.roesch@epitech.eu>
2023-09-29 16:16:35 +0200

    Add v0 script

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+import smtplib
+
+from send_mail_to_users import send_mail_to_users
+
+email_sender = ''
+app_password = ''
+
+
+def get_csv_filepath():
+    """Prompt the user for a CSV file path and validate the input.
+
+    Returns:
+        str: The validated CSV file path.
+    """
+    while True:
+        filepath = input("Please enter the path to the CSV file: ").strip()
+        if os.path.isfile(filepath) and filepath.endswith('.csv'):
+            return filepath
+        else:
+            print("Invalid file path or file type. Please enter a valid CSV file path.")
+
+
+def authenticate_user():
+    while True:
+        global email_sender
+        global app_password
+        email_sender = input("Please enter your Google email: ")
+        app_password = input("Please enter your app password: ")
+
+        try:
+            # Attempt to log in to the SMTP service using the provided credentials
+            with smtplib.SMTP_SSL('smtp.gmail.com') as server:
+                server.login(email_sender, app_password)
+
+            # If the login is successful, the credentials are valid
+            print("Authentication successful. Proceeding...")
+            break
+        except smtplib.SMTPAuthenticationError:
+            # If authentication fails, an SMTPAuthenticationError is raised
+            print("Authentication failed. Please verify your email and app password and try again.")
+
+
+def main():
+    print("\033[91mALERT: You are about to use a tool that interacts with the live production environment.")
+    print("Misuse can have serious repercussions. Proceed with utmost caution and responsibility.")
+    print("We strongly encourage you to read the README file before using the program if you haven't done so already."
+          "\033[0m\n")
+
+    authenticate_user()
+
+    while True:
+        try:
+            print("Choose an option:")
+            print("1: Send emails to users")
+            print("2: Quit (q)")
+
+            option = input("Enter your choice: ").strip()
+
+            if option in {"1", "2", "q"}:
+                if option == "1":
+                    csv_filepath = get_csv_filepath()
+                    confirmation = input(f"You chose option {option}. Are you sure? (y/n): ").strip()
+                    if confirmation == 'y':
+                        send_mail_to_users(csv_filepath, email_sender, app_password)
+                    else:
+                        print("Option not confirmed, please try again.")
+                elif option in {"2", "q"}:
+                    print("Goodbye!")
+                    break
+            else:
+                print("Invalid option. Please enter a valid option (1, or 2/q).")
+        except ValueError:
+            print("Invalid input. Please enter a number (1 or 2) or 'q'.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ send_mail_to_users.py
@@ -0,0 +1,201 @@
+import csv
+import json
+import smtplib
+import ssl
+import time
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def load_csv_data(filename):
+    """
+    Load the CSV data from the specified file.
+
+    :param filename: The name of the file to load the data from.
+    :return: The loaded CSV data as a list of dictionaries.
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            data = [row for row in reader]
+        return data
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return None
+    except csv.Error:
+        print(f"Could not decode CSV from file {filename}.")
+        return None
+
+
+def check_email_progress(user_data):
+    try:
+        with open('send_progress.json', 'r', encoding='utf-8') as file:
+            progress_data = json.load(file)
+    except FileNotFoundError:
+        progress_data = []
+
+    users_to_email = {}
+    for user in user_data:
+        email = user['email']
+        if not any(d['email'] == email and d['email_sent'] for d in progress_data):
+            users_to_email[email] = user
+    return users_to_email
+
+
+def save_sending_progress(data):
+    try:
+        with open('send_progress.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+    except Exception as e:
+        print(f"Failed to update JSON file: {e}")
+
+
+def create_email_message(receiver_email, sender_email, forename, surname):
+    """Create an email message with both plaintext and HTML content.
+
+    Args:
+        receiver_email (str): The recipient's email address.
+        sender_email (str): The email of the sender.
+        forename (str): The recipient's forename.
+        surname (str): The recipient's surname.
+
+    Returns:
+        MIMEMultipart: The email message object.
+    """
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Vos informations de connexion à SAFETROOPER"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+
+    text = """\
+    Bonjour {0} {1},
+    Vous trouverez ci-dessous vos identifiants pour accéder au jeu SAFETROOPER :
+    Identifiant :
+    Mot de passe :
+    Connectez-vous en utilisant le lien suivant : https://app.safetrooper.com/
+    Bonne journée,
+    L'Équipe SafeTrooper
+    """
+    html = """\
+    <html>
+      <body>
+        <p>Bonjour {0} {1},</p>
+        <p>Vous trouverez ci-dessous vos identifiants pour accéder au jeu SAFETROOPER :</p>
+        <ul>
+            <li><strong>Identifiant</strong>: </li>
+            <li><strong>Mot de passe</strong>: </li>
+        </ul>
+        <p>Connectez-vous en utilisant le lien suivant : <a href="https://app.safetrooper.com/">
+            https://app.safetrooper.com/</a></p>
+        <p>Bonne journée,</p>
+        <p>L'Équipe SafeTrooper</p>
+      </body>
+    </html>
+    """
+
+    formated_text = text.format(forename, surname)
+    formated_html = html.format(forename, surname)
+
+    part1 = MIMEText(formated_text, "plain")
+    part2 = MIMEText(formated_html, "html")
+
+    message.attach(part1)
+    message.attach(part2)
+
+    return message
+
+
+def send_email(email_sender, app_password, receiver_email, message):
+    """Send the email message to the recipient.
+
+    Args:
+        email_sender (str): The email address of the sender.
+        app_password (str): The app password for the sender's email address.
+        receiver_email (str): The recipient's email address.
+        message (MIMEMultipart): The email message object.
+
+    Returns:
+        bool: True if the email was sent successfully, False otherwise.
+    """
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = email_sender
+    password_email = app_password
+
+    context = ssl.create_default_context()
+    try:
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password_email)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+        return True
+    except Exception as e:
+        print(f"\nCould not send email to {receiver_email} due to {str(e)}")
+        return False
+
+
+def send_mail_to_users(csv_filename, email_sender, app_password):
+    """Send emails to users from the data loaded from the JSON file.
+
+    :param csv_filename: The name of the CSV file containing the user email address.
+    :param email_sender: The email address of the sender.
+    :param app_password: The app password for the sender's email address.
+
+    """
+    # Load the JSON data
+    user_data = load_csv_data(csv_filename)
+
+    # Check the email status for each user
+    users_to_email = check_email_progress(user_data)
+
+    # Create and send emails to users who haven't received emails yet
+    email_counter = 0
+    email_success = 0
+    batch_size = 80
+    delay_between_batches = 600  # 10 minutes in seconds
+    total_emails = len(users_to_email)
+
+    # Create and send emails to users who haven't received emails yet
+    for email, user_details in users_to_email.items():
+        receiver_email = email
+        forename = user_details.get("forename")
+        surname = user_details.get("surname")
+
+        # Create the email message
+        message = create_email_message(receiver_email, email, forename, surname)
+
+        # Send the email
+        email_sent = send_email(email_sender, app_password, receiver_email, message)
+
+        # Update the emailSent status for the user
+        user_details["email_sent"] = email_sent
+
+        if email_sent:
+            email_success += 1
+
+        # Save the progress immediately
+        save_sending_progress(user_data)
+
+        # Print a message if the email failed to send
+        if not email_sent:
+            print(f"\nFailed to deliver message to user: {email}")
+
+        # Update the email counter
+        email_counter += 1
+
+        # Display the email counter and the number of emails left
+        print(f"\rEmails sent: {email_success}/{total_emails}, Emails left: {total_emails - email_counter}", end="")
+
+        # If the batch size is reached, wait for 10 minutes before the next batch
+        if email_counter % batch_size == 0:
+            print(f"\nSent {batch_size} emails, waiting for 10 minutes before sending the next batch...")
+
+            # Display a countdown
+            for remaining in range(delay_between_batches, 0, -1):
+                mins, secs = divmod(remaining, 60)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                print(f"\r{timer} before the next batch, hang tight...", end="")
+                time.sleep(1)
+
+    # Print a completion message once all emails have been sent
+    print(f"\n\033[92mAll emails have been sent! Total emails sent: {email_success}/{total_emails}.\033[0m")
